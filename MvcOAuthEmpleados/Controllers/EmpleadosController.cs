4cff9b3990a3fdb45dd43de7a6ed5d03bc732582
serraguti <pacoserranox@gmail.com>
2025-03-28 11:51:16 +0100

    Add project files.

---
+++ MvcOAuthEmpleados/Controllers/EmpleadosController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MvcOAuthEmpleados.Filters;
+using MvcOAuthEmpleados.Models;
+using MvcOAuthEmpleados.Services;
+using System.Security.Claims;
+
+namespace MvcOAuthEmpleados.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private ServiceEmpleados service;
+
+        public EmpleadosController(ServiceEmpleados service)
+        {
+            this.service = service;
+        }
+
+        [AuthorizeEmpleados]
+        public async Task<IActionResult> Index()
+        {
+            List<Empleado> empleados =
+                await this.service.GetEmpleadosAsync();
+            return View(empleados);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            Empleado empleado = await
+                    this.service.FindEmpleadoAsync(id);
+            return View(empleado);
+        }
+    }
+}
---
+++ MvcOAuthEmpleados/Controllers/ManagedController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using MvcOAuthEmpleados.Models;
+using MvcOAuthEmpleados.Services;
+using System.Security.Claims;
+
+namespace MvcOAuthEmpleados.Controllers
+{
+    public class ManagedController : Controller
+    {
+        private ServiceEmpleados service;
+
+        public ManagedController(ServiceEmpleados service)
+        {
+            this.service = service;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            string token = await this.service
+                .GetTokenAsync(model.UserName, model.Password);
+            if (token == null)
+            {
+                ViewData["MENSAJE"] = "Usuario/Password incorrectos";
+                return View();
+            }
+            else
+            {
+                ViewData["MENSAJE"] = "Ya tienes tu Token!!!";
+                HttpContext.Session.SetString("TOKEN", token);
+                ClaimsIdentity identity =
+                    new ClaimsIdentity
+                    (CookieAuthenticationDefaults.AuthenticationScheme
+                    , ClaimTypes.Name, ClaimTypes.Role);
+                //ALMACENAMOS EL NOMBRE DEL USUARIO (BONITO)
+                identity.AddClaim(new Claim
+                    (ClaimTypes.Name, model.UserName));
+                //EN ESTE EJEMPLO, ALMACENAMOS EL ID (PASSWORD)
+                identity.AddClaim(new Claim
+                    (ClaimTypes.NameIdentifier, model.Password));
+                //ALMACENAMOS EL TOKEN EN EL USUARIO
+                identity.AddClaim(new Claim
+                    ("TOKEN", token));
+                ClaimsPrincipal principal =
+                    new ClaimsPrincipal(identity);
+                //EL USUARIO ESTARA DADO DE ALTA 30 MINUTOS
+                //, LO MISMO QUE SESSION 
+                await HttpContext.SignInAsync
+                    (CookieAuthenticationDefaults.AuthenticationScheme
+                    , principal, new AuthenticationProperties
+                    {
+                        ExpiresUtc = DateTime.UtcNow.AddMinutes(30)
+                    });
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync
+                (CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
